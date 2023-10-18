588b3bdcfc5eca591b8c6e6c310efaae21f42193
flaminshinjan <srikpatra.2008@gmail.com>
2023-10-18 17:42:36 +0530

    Initial Commit

---
+++ bar_implementation/bar_implementationApp.swift
@@ -0,0 +1,17 @@
+//
+//  bar_implementationApp.swift
+//  bar_implementation
+//
+//  Created by Shinjan Patra on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct bar_implementationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
